5249c1cf4299ea19ffd7effe00e48bd25b1f9a55
Rakesh Choudhary <seervir987@gmail.com>
2025-08-03 17:21:26 +0530

    final changes

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.modal.js";
-import ApiError from "../utils/ApiError.js";
-import asyncHandler from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 export const authMiddleware = asyncHandler(async (req, _, next) => {
   try {
